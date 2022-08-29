a357abc845318459ea6b76f9b11e6fcf2bad5893
ReddyG03 <100753868+ReddyG03@users.noreply.github.com>
2022-08-30 02:15:14 +0530

    Add files via upload

---
+++ JAVA/h1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Sum{
+    public static void main(String[] args) {
+        int x,y,sum;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Typer a Number:");
+        x = obj.nextInt();
+
+
+        System.out.println("Type another no:");
+        y = obj.nextInt();
+
+        sum = x+y;
+        System.out.println("Sum is:" + sum);
+    }
+}
